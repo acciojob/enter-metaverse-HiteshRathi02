95353bbbb10e2ce51c4adbe5563e034442f11794
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 13:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const text = document.getElementById("text");
+const text = document.getElementById("status");
 const btn = document.getElementById("enterBtn");
 
 btn.addEventListener("click", function() {
