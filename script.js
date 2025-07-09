ddcd94bca66fedd4831c2e99d5f263efa604b965
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 12:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const btn = document.getElementById("enterbtn");
+
+btn.addEventListener("click", function() {
+    text.innerHTML = `<h1>Entered Metaverse</h1>`;
+})
